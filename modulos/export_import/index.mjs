9328907a71e7778fe0631f7743159e69908c63cf
odevpedro <pedrosschmidt2@gmail.com>
2022-06-20 11:51:59 -0300

    trabalhando com modulos internos

---
+++ modulos/export_import/index.mjs
@@ -0,0 +1,6 @@
+import soma from './meu_modulo.mjs'
+
+//const meuModulo = require('./importando')
+//const soma = meuModulo.soma
+
+soma(1,2)
---
+++ modulos/modulo_interno/index.js
@@ -0,0 +1,7 @@
+const meuModulo = require('./importando')
+const soma = meuModulo.soma
+
+soma(2, 3)
+soma(8, 8)
+
+meuModulo.soma(100, 145)
