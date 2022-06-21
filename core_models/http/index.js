6b170bb551d883a4326857a6cb20d5e049d1fb83
odevpedro <pedrosschmidt2@gmail.com>
2022-06-21 12:04:05 -0300

    modulo de http

---
+++ core_models/http/index.js
@@ -0,0 +1,11 @@
+const http = require('http')
+
+const port = 3000
+const server = http.createServer( (req, res)=> {
+res.write('Hello world')
+res.end()
+})
+
+server.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
